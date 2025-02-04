0c612d4685698b9774b11901ff416532b31ad32d
agamyo168 <agamyo168@gmail.com>
2025-02-04 03:26:50 +0200

    setup nest-js repo with nest playground

---
+++ tutorial-hell/src/users/user.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/dtos/create-user.dto';
+import { UpdateUserDto } from 'src/dtos/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  // private readonly user: UserEntity[] = [];
+  //@Method -> HTTP METHOD
+  @Get() // this by default watches /users/ since it's declared in @Controller
+  findAll(): string[] {
+    return ['Ahmed', 'Khaled', 'Fatma'];
+  }
+  @Get(':username') //Parameter -> dynamic id -> :id
+  findOne(@Param('username') username: string): string {
+    return username;
+  }
+
+  // create(@Req() req: Request): string {
+  //   console.log(req.body);
+  //}
+  @Post('new') // URL: /users/new route
+  create(@Body() createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+  @Patch(':username')
+  update(@Body() updateUserDto: UpdateUserDto): string {
+    return `Update user ${updateUserDto.username}`;
+  }
+
+  @Delete(':username')
+  remove(@Param('username') username: string): string {
+    return `removed username ${username}`;
+  }
+}
---
+++ tutorial-hell/src/users/user.entity.ts
@@ -0,0 +1,5 @@
+export class UserEntity {
+  readonly id: string;
+  readonly email: string;
+  readonly country: string;
+}
