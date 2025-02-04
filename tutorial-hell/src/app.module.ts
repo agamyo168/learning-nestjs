695dd708bce9a62fbfc609b12fe9b3635b5ab22b
agamyo168 <agamyo168@gmail.com>
2025-02-04 04:07:44 +0200

    fix folder structure

--- tutorial-hell/src/app.module.ts
+++ tutorial-hell/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/user.module';
+import { OrdersModule } from './orders/orders.module';
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, OrdersModule],
 })
 export class AppModule {}
--- tutorial-hell/src/users/user.controller.ts
+++ tutorial-hell/src/users/user.controller.ts
@@ -7,8 +7,8 @@ import {
   Param,
   Body,
 } from '@nestjs/common';
-import { CreateUserDto } from 'src/dtos/create-user.dto';
-import { UpdateUserDto } from 'src/dtos/update-user.dto';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { UpdateUserDto } from 'src/users/dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
